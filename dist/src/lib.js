d7711b4d246bef8c253ac57343cd33ac48e8818f
he jiajun <hejiajun@hedeMacBook-Pro.local>
2023-12-05 12:20:47 +0800

    feat: 新增设备通道信息同步
    feat: 新增设备通道信息同步

--- dist/src/lib.js
+++ dist/src/lib.js
@@ -794,6 +794,29 @@ class WcsSdk {
         return msg_id;
     }
     /**
+     * 同步设备下通道信息
+     * @param device_path 网关path 例:/dist_15/link_1/2000000000
+     * @param uuid 设备uuid 例:b4dd74da94ac63b96b0a906393ae8c69
+     * @returns
+     */
+    async sync_channels(device_path, uuid) {
+        const msg_id = this.getMsgId();
+        let req_body = {
+            namespace: "WCS/main",
+            request: "query.vbox_config",
+            msg_id: msg_id,
+            content: {
+                command: "_sync_channels",
+                params: {
+                    id: uuid,
+                },
+                device_path
+            }
+        };
+        this.exec(req_body);
+        return msg_id;
+    }
+    /**
      * 主动搜索网关下局域网设备
      * @param device_path
      * @returns
